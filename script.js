00f557c7a31dbd46c3df9c92f76dc705b1c7885d
Braian <braianvoficial@gmail.com>
2022-05-29 15:41:21 -0300

    adicionando cores

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const POKEMONS_COLORS = {
+    "bulbasaur":"#68A890","ivysaur":"#498899","venusaur":"#489180","charmander":"#FFD57B","charmeleon":"#FC6C6D",
+    "caterpie":"#6AD531","squirtle":"#76BEFE","metapod":"#5bbb27","wartortle":"#627BC5","weedle":"#EEAC41",
+    "blastoise":"#5A8BCD","charizard":"#EE8329","kakuna":"#F6CD31","beedrill":"#DECDF6","pidgey":"#CD835A",
+    "pidgeotto":"#AC7B5A","butterfree":"#8383BD","pidgeot":"#E6B462","spearow":"#EEAC52","fearow":"#EDD3B0",
+    "raichu":"#FFD43E","nidorina":"#C5E6EE","rattata":"#D59CD5","nidorino":"#CD83DE","raticate":"#C59439",
+    "ninetales":"#E6C552","ekans":"#EEA4D5","oddish":"#9CD54A","arbok":"#A483C5","pikachu":"#F6E652",
+    "gloom":"#7B83A4","vileplume":"#5A6283","sandshrew":"#EEDE10","venonat":"#A48BBD",
+    "venonat":"#A48BBD",
+};
+
 var quantidade = document.getElementById('quantidade')
 quantidade.addEventListener('keyup', function(){
     pegaPokemons(quantidade.value);
@@ -27,9 +38,15 @@ function pegaPokemons(quantidade){
                         pokemon_boxes.innerHTML = "";
 
                             pokemons.map(function(val){
+                            var color = "";
+                            if(POKEMONS_COLORS[val.nome] !== undefined) {
+                                color = POKEMONS_COLORS[val.nome];
+                            } else {
+                                color = "#ccc";
+                            }
                             pokemon_boxes.innerHTML += `
 
-                                <div class="pokemon-box">
+                                <div class="pokemon-box" style="background-color: `+color+`">
                                     <img src="`+val.imagem+`" />
                                     <p>`+val.nome+`</p>
                                 </div>
